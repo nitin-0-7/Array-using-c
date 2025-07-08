9f2059fc37b937c8b500955e66d20be581d91723
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-08 18:44:58 +0530

    printing input and taking output

---
+++ printing input and taking output.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    printf("Enter first number ");
+    scanf("%d",&arr[0]);
+    printf("Enter second number ");
+    scanf("%d",&arr[1]);
+    printf("Enter third number ");
+    scanf("%d",&arr[2]);
+    printf("Enter fourth number ");
+    scanf("%d",&arr[3]);
+    printf("Enter fifth number ");
+    scanf("%d",&arr[4]);
+    printf("%d",arr[3]);
+    return 0;
+}
