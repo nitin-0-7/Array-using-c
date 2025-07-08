713a76f0cf59e5682d8ba7fad880cdd25c3d7831
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-08 18:23:58 +0530

    output and input using loops

---
+++ output and input using loops.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    float arr[5]={4.5,3.2,4.4,8.7,3.2};
+    for(int i=0;i<=5;i++)
+    
+    printf("%f\n",arr[i]);
+
+    return 0;
+}
