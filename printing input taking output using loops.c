6b0e8e128b8f785129365986b0fd86ed78b5a6d2
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-10 10:59:22 +0530

    printing input taking output using loops.c

---
+++ printing input taking output using loops.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    for(int i=0;i<=5;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("%d ",arr[3]);
+    
+    
+    return 0;
+}
