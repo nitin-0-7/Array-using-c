44c18aa2acc93c2906c183b40da086f2afaf7837
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-10 14:19:39 +0530

    another way to declare an array

---
+++ another way to declare an array .c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int size=10,b[size];
+    printf("%d\n",size);
+    b[5]=3;
+    printf("%d",b[5]);
+
+    return 0;
+}
