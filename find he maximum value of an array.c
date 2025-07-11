043bea112e9c18e27e75be89cafd002ec30882b4
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-11 11:01:26 +0530

    find he maximum value of an array

---
+++ find he maximum value of an array.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[9]={18,45,65,23,45,13,11,3,16};
+    int max=arr[0];
+    for(int i=1;i<=8;i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    printf("The maximum value of an array is %d ",max);
+    return 0;
+}
