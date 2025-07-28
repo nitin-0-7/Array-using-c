e60933205a9dbac31dbb66cb936539b01e1ea02f
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-28 16:34:21 +0530

    Update find duplicate element in the array.c

--- find duplicate element in the array.c
+++ find duplicate element in the array.c
@@ -11,10 +11,11 @@ Write your code in this editor and press "Run" button to compile and execute it.
 int main()
 {
     int arr[8]={2,3,4,2,3,4,5,6};
+  printf("These are the duplicate element in the array\n");
     for(int i=0;i<=7;i++){
         for(int j=i+1;j<=7;j++){
             if(arr[i]==arr[j]){
-                printf("These are the duplicate element in the array %d ",arr[i]);
+                printf("%d ",arr[i]);
             }
         }
 }
